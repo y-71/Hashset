d4beda3bb5f4b4ea386fb6e1821086de717d5d70
tawfik boujeh <tboujeh@gmail.com>
2023-11-04 01:12:21 +0100

    ok, good enough

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
 )
 
 const (
 	Prime              = (1 << 31) - 1
 	a                  = 5
-	InitialHashsetSize = 16
+	DefaultHashsetSize = 16
 )
 
 type Chain []string
@@ -22,8 +21,17 @@ type Hashset struct {
 
 func New() Hashset {
 	return Hashset{
-		size:     InitialHashsetSize,
-		elements: make([]Chain, InitialHashsetSize),
+		size:     DefaultHashsetSize,
+		elements: make([]Chain, DefaultHashsetSize),
+		len:      uint32(0),
+	}
+}
+
+func Copy(h Hashset) Hashset {
+	return Hashset{
+		size:     h.size,
+		elements: h.elements,
+		len:      h.len,
 	}
 }
 
@@ -36,6 +44,109 @@ func (c Chain) contains(target string) bool {
 	return false
 }
 
+func (c Chain) remove(target string) []string {
+	for i, el := range c {
+		if el == target {
+			return append(c[:i], c[i+1:]...)
+		}
+	}
+	return c
+}
+
+func (h *Hashset) Insert(element string) {
+	if h.Has(element) {
+		return
+	}
+
+	index := h.hash(element)
+	h.elements[index] = append(h.elements[index], element)
+
+	h.len++
+
+	if h.len > h.size {
+		h.size = h.size * 2
+		newElements := make([]Chain, h.size)
+		for _, el := range h.flatten() {
+			index := h.hash(el)
+			newElements[index] = append(newElements[index], el)
+		}
+		h.elements = newElements
+	}
+}
+
+func (h *Hashset) Remove(element string) {
+	if !h.Has(element) {
+		return
+	}
+
+	index := h.hash(element)
+	h.elements[index] = h.elements[index].remove(element)
+
+	h.len -= 1
+
+	if h.len < h.size/4 && h.size/4 >= DefaultHashsetSize {
+		h.size = h.size / 4
+		newElements := make([]Chain, h.size)
+		for _, el := range h.flatten() {
+			index := h.hash(el)
+			newElements[index] = append(newElements[index], el)
+		}
+		h.elements = newElements
+	}
+}
+
+func (h Hashset) Has(element string) bool {
+	index := h.hash(element)
+	return h.elements[index].contains(element)
+}
+
+func (x Hashset) Intersect(y Hashset) Hashset {
+	var intersection = New()
+
+	for _, el := range y.flatten() {
+		if x.Has(el) {
+			intersection.Insert(el)
+		}
+	}
+
+	return intersection
+}
+
+func (x Hashset) Union(y Hashset) Hashset {
+	union := Copy(x)
+
+	for _, el := range y.flatten() {
+		union.Insert(el)
+	}
+
+	return union
+}
+
+func (x Hashset) Superset(y Hashset) bool {
+	intersection := x.Intersect(y)
+	if(intersection.len != y.len) {return false}
+	for _, el := range(intersection.flatten()){
+		if(!y.Has(el)){return false}
+	}
+	return true
+}
+
+func (x Hashset) Subset(y Hashset) bool {
+	return y.Superset(x)
+}
+
+func (h Hashset) Println() {
+	fmt.Println(h.flatten())
+}
+
+func (h Hashset) flatten() []string {
+	var flatArray []string
+	for _, el := range h.elements {
+		flatArray = append(flatArray, el...)
+	}
+	return flatArray
+}
+
 func (h Hashset) hash(element string) uint32 {
 	hash := uint32(0)
 
@@ -45,55 +156,19 @@ func (h Hashset) hash(element string) uint32 {
 	return hash % h.size
 }
 
-func (h Hashset) Add(element string) {
-	if h.Has(element) {
-		return
-	}
-	h.len += 1
-	if h.len < h.size {
-		index := h.hash(element)
-		if len(h.elements[index]) == 0 {
-			h.elements[index] = []string{element}
-		} else {
-			h.elements[index] = append(h.elements[index], element)
-		}
-	} else {
-		h.size = h.size * 2
-		newElements := make([]Chain, h.size)
-		for _, el := range(h.flatten()){
-			index := h.hash(el)
-			if len(newElements[index]) == 0 {
-				newElements[index] = []string{el}
-			} else {
-				newElements[index] = append(newElements[index], el)
-			}
-		}
-	}
-}
-
-func (h Hashset)flatten() []string{
-	output := fmt.Sprint(h.elements)
-	output = strings.ReplaceAll(output, "[", "")
-	output = strings.ReplaceAll(output, "]", "")
-	return strings.Split(output, " ")
-}
-
-func (h Hashset) Println() {
-	fmt.Println(h.flatten())
-}
-
-func (h Hashset) Has(element string) bool {
-	index := h.hash(element)
-	return h.elements[index].contains(element)
-}
-
 func main() {
 	hashset := New()
 
-	for i := 0; i < 32; i++ {
+	for i := 0; i < 63; i++ {
 		el := strconv.Itoa(i)
-		fmt.Println(hashset.hash(el))
-		hashset.Add(el)
+		hashset.Insert(el)
 	}
+	for i := 0; i < 32+17; i++ {
+		el := strconv.Itoa(i)
+		hashset.Remove(el)
+	}
+
 	hashset.Println()
+	fmt.Println("hashset len:", hashset.len)
+	fmt.Println("hashset size:", hashset.size)
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,17 @@
 package main
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
 	hashset := New()
-	if hashset.size != InitialHashsetSize {
-		t.Errorf("Expected size %d, got %d", InitialHashsetSize, hashset.size)
+	if hashset.size != DefaultHashsetSize {
+		t.Errorf("Expected size %d, got %d", DefaultHashsetSize, hashset.size)
 	}
-	if len(hashset.elements) != int(InitialHashsetSize) {
-		t.Errorf("Expected length %d, got %d", InitialHashsetSize, len(hashset.elements))
+	if len(hashset.elements) != int(DefaultHashsetSize) {
+		t.Errorf("Expected length %d, got %d", DefaultHashsetSize, len(hashset.elements))
 	}
 }
 
@@ -18,7 +21,7 @@ func TestHashFunction(t *testing.T) {
 		expected uint32
 	}{
 		{"test", 4}, // Insert expected hash value for "test" after calculating it
-		// Add more test cases for hash function here
+		// Insert more test cases for hash function here
 	}
 
 	hashset := New()
@@ -31,22 +34,208 @@ func TestHashFunction(t *testing.T) {
 	}
 }
 
-func TestAddAndHas(t *testing.T) {
+func TestInsertAndHas(t *testing.T) {
 	hashset := New()
 
-	// Add test cases to validate adding elements and checking existence
-	testAdd := "example"
-	hashset.Add(testAdd)
+	// Insert test cases to validate Inserting elements and checking existence
+	testInsert := "example"
+	hashset.Insert(testInsert)
 
-	if !hashset.Has(testAdd) {
-		t.Errorf("Expected %s to be present in the hashset, but it's not", testAdd)
+	if !hashset.Has(testInsert) {
+		t.Errorf("Expected %s to be present in the hashset, but it's not", testInsert)
 	}
 
-	testNotAdded := "non-existent"
-	if hashset.Has(testNotAdded) {
-		t.Errorf("Expected %s to not be present in the hashset, but it is", testNotAdded)
+	testNotInserted := "non-existent"
+	if hashset.Has(testNotInserted) {
+		t.Errorf("Expected %s to not be present in the hashset, but it is", testNotInserted)
 	}
 }
+
+func TestRemoveAndHas(t *testing.T) {
+	hashset := New()
+
+	// Insert elements into the hashset
+	for i := 0; i < 5; i++ {
+		el := strconv.Itoa(i)
+		hashset.Insert(el)
+	}
+
+	// Remove elements from the hashset
+	for i := 0; i < 3; i++ {
+		el := strconv.Itoa(i)
+		hashset.Remove(el)
+	}
+
+	// Test if removed elements don't exist in the hashset
+	for i := 0; i < 3; i++ {
+		el := strconv.Itoa(i)
+		if hashset.Has(el) {
+			t.Errorf("Expected %s to be removed from the hashset, but it's still present.", el)
+		}
+	}
+
+	// Test if elements that were not removed are still present in the hashset
+	for i := 3; i < 5; i++ {
+		el := strconv.Itoa(i)
+		if !hashset.Has(el) {
+			t.Errorf("Expected %s to be in the hashset, but it's not.", el)
+		}
+	}
+}
+
+func TestCopy(t *testing.T) {
+	hashsetX := New()
+
+	// Insert elements into the hashset
+	for i := 0; i < 5; i++ {
+		el := strconv.Itoa(i)
+		hashsetX.Insert(el)
+	}
+
+	// Create a copy of the hashset
+	copyHashset := Copy(hashsetX)
+
+	// Test if the copied hashset is equal to the original hashset
+	for i := 0; i < 5; i++ {
+		el := strconv.Itoa(i)
+		if !copyHashset.Has(el) {
+			t.Errorf("Expected %s to be in the copied hashset, but it's not.", el)
+		}
+	}
+}
+
+func TestRemoveNonExistentElement(t *testing.T) {
+	hashset := New()
+
+	// Insert elements into the hashset
+	for i := 0; i < 5; i++ {
+		el := strconv.Itoa(i)
+		hashset.Insert(el)
+	}
+
+	// Remove an element that doesn't exist in the hashset
+	nonExistentElement := "10"
+	hashset.Remove(nonExistentElement)
+
+	// Test if the non-existent element removal did not affect the hashset
+	if hashset.len != 5 {
+		t.Errorf("Expected hashset length to remain the same after removing a non-existent element.")
+	}
+}
+
+func TestEmptyHashsetIntersection(t *testing.T) {
+	hashsetX := New()
+	hashsetY := New()
+
+	// Test intersection of two empty hashsets
+	intersection := hashsetX.Intersect(hashsetY)
+
+	// Test if the intersection of two empty hashsets is also an empty hashset
+	if intersection.len != 0 {
+		t.Errorf("Expected the intersection of two empty hashsets to be empty, but it's not.")
+	}
+}
+
+func TestEmptyHashsetUnion(t *testing.T) {
+	hashsetX := New()
+	hashsetY := New()
+
+	// Test union of two empty hashsets
+	union := hashsetX.Union(hashsetY)
+
+	// Test if the union of two empty hashsets is also an empty hashset
+	if union.len != 0 {
+		t.Errorf("Expected the union of two empty hashsets to be empty, but it's not.")
+	}
+}
+
+func TestEmptySubsetAndSuperset(t *testing.T) {
+	hashsetX := New()
+	hashsetY := New()
+
+	// Test if an empty hashset is both a subset and a superset of another empty hashset
+	if !hashsetX.Subset(hashsetY) || !hashsetY.Superset(hashsetX) {
+		t.Errorf("Expected an empty hashset to be both a subset and a superset of another empty hashset, but it's not.")
+	}
+}
+
+func TestEmptyHashsetHas(t *testing.T) {
+	hashset := New()
+
+	// Test if an element exists in an empty hashset
+	if hashset.Has("0") {
+		t.Errorf("Expected an empty hashset not to have any elements.")
+	}
+}
+
+
+func TestHashCollision(t *testing.T) {
+	hashset := New()
+
+	// Force hash collision by inserting elements with the same hash
+	// This test checks how the Hashset handles hash collisions
+	hashset.Insert("a")
+	hashset.Insert("b")
+	hashset.Insert("aa") // This should hash to the same index as "a"
+
+	if !hashset.Has("a") || !hashset.Has("b") || !hashset.Has("aa") {
+		t.Errorf("Expected all elements to be present after hash collision, but some are missing.")
+	}
+}
+
+func TestIntersect(t *testing.T) {
+	hashsetX := New()
+	hashsetY := New()
+
+	// Insert elements into the hashsets
+	for i := 0; i < 5; i++ {
+		el := strconv.Itoa(i)
+		hashsetX.Insert(el)
+	}
+	for i := 3; i < 8; i++ {
+		el := strconv.Itoa(i)
+		hashsetY.Insert(el)
+	}
+
+	// Get the intersection of hashsetX and hashsetY
+	intersection := hashsetX.Intersect(hashsetY)
+
+	// Test if the intersection contains elements that are common in both hashsets
+	for i := 3; i < 5; i++ {
+		el := strconv.Itoa(i)
+		if !intersection.Has(el) {
+			t.Errorf("Expected %s to be in the intersection, but it's not.", el)
+		}
+	}
+}
+
+func TestUnion(t *testing.T) {
+	hashsetX := New()
+	hashsetY := New()
+
+	// Insert elements into the hashsets
+	for i := 0; i < 5; i++ {
+		el := strconv.Itoa(i)
+		hashsetX.Insert(el)
+	}
+	for i := 3; i < 8; i++ {
+		el := strconv.Itoa(i)
+		hashsetY.Insert(el)
+	}
+
+	// Get the union of hashsetX and hashsetY
+	union := hashsetX.Union(hashsetY)
+
+	// Test if the union contains all elements from both hashsets
+	for i := 0; i < 8; i++ {
+		el := strconv.Itoa(i)
+		if !union.Has(el) {
+			t.Errorf("Expected %s to be in the union, but it's not.", el)
+		}
+	}
+}
+
+
 
 func TestMainFunction(t *testing.T) {
 	// This test covers the main function for basic functionality
